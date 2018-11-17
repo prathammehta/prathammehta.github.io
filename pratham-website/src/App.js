1fd25719230de7b8feefd5478e570a679738a27e
Pratham Mehta <prathammehta@outlook.com>
2018-11-17 17:13:51 +0530

    Moved to ReactJs. Needs a lot more work. But it's a good start.

---
+++ pratham-website/src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p className="App-para">Hello,</p>
+          <p className="App-para">I'm Pratham Mehta. Nice to meet you.</p>
+          <p className="App-para">I make amazing apps.</p>
+          <p className="App-para">Got an idea you'd like to see turned into an app?</p>
+          <p><a className="App-anchor" href="mailto:me@prath.am">Let me know!</a></p>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ precache-manifest.3ba4b1831140fe4678cc03a2bd4fabad.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "afc8c73f8d723af8d2e9",
+    "url": "/static/css/main.3020e0e3.chunk.css"
+  },
+  {
+    "revision": "afc8c73f8d723af8d2e9",
+    "url": "/static/js/main.afc8c73f.chunk.js"
+  },
+  {
+    "revision": "f68dcc1a0c0c6e129be0",
+    "url": "/static/js/1.f68dcc1a.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3086dbe307f18645eb70d65656dcab60",
+    "url": "/index.html"
+  }
+];

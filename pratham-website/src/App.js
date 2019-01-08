d30f7158c84e6773025df96fa968678e16385fbf
Pratham Mehta <prathammehta@outlook.com>
2019-01-08 15:59:51 +0530

    Title changed

--- pratham-website/src/App.js
+++ pratham-website/src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <p className="App-para">Hello,</p>
+          <p className="App-para">Hi there,</p>
           <p className="App-para">I'm Pratham Mehta. Nice to meet you.</p>
           <p className="App-para">I make amazing apps.</p>
           <p className="App-para">Got an idea you'd like to see turned into an app?</p>
